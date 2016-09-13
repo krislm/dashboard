28e3bae5673cdb5248ef47c6ca5ebfc2539db3dd
mcabe <mcabe@me.com>
2016-09-13 23:42:50 +0200

    initial

---
+++ www/js/app.js
@@ -0,0 +1,94 @@
+// Ionic template App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'SimpleRESTIonic' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('SimpleRESTIonic', ['ionic', 'backand', 'SimpleRESTIonic.controllers', 'SimpleRESTIonic.services'])
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
+    })
+    .config(function (BackandProvider, $stateProvider, $urlRouterProvider, $httpProvider) {
+
+        BackandProvider.setAppName('familydashboard'); // change here to your app name
+        BackandProvider.setSignUpToken('d2fc44ee-b348-4a56-9873-e0dd5946f344'); //token that enable sign up. see http://docs.backand.com/en/latest/apidocs/security/index.html#sign-up
+        BackandProvider.setAnonymousToken('2673125e-fd17-417b-bb26-ca256b5c3132'); // token is for anonymous login. see http://docs.backand.com/en/latest/apidocs/security/index.html#anonymous-access
+
+        $stateProvider
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tabs',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+            .state('tab.dashboard', {
+                url: '/dashboard',
+                views: {
+                    'tab-dashboard': {
+                        templateUrl: 'templates/tab-dashboard.html',
+                        controller: 'DashboardCtrl as dashboard'
+                    }
+                }
+            })
+            .state('tab.login', {
+                url: '/login',
+                views: {
+                    'tab-login': {
+                        templateUrl: 'templates/tab-login.html',
+                        controller: 'LoginCtrl as login'
+                    }
+                }
+            })
+            .state('tab.post', {
+                  url: '/post',
+                  views: {
+                      'tab-post': {
+                          templateUrl: 'templates/tab-post.html',
+                          controller: 'PostCtrl as post'
+                      }
+                  }
+            });
+
+        $urlRouterProvider.otherwise('/tabs/dashboard');
+
+        $httpProvider.interceptors.push('APIInterceptor');
+    })
+
+    .run(function ($rootScope, $state, LoginService, Backand) {
+
+        function unauthorized() {
+            console.log("user is unauthorized, sending to login");
+            $state.go('tab.login');
+        }
+
+        function signout() {
+            LoginService.signout();
+        }
+
+        $rootScope.$on('unauthorized', function () {
+            unauthorized();
+        });
+
+        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+            if (toState.name == 'tab.login') {
+                signout();
+            }
+            else if (toState.name != 'tab.login' && Backand.getToken() === undefined) {
+                unauthorized();
+            }
+        });
+
+    })
+
